c7ebf185a6f33c49e9a24ac4327c212f9aa05322
Jayanth P <jayanthjayram6968@gmail.com>
2024-12-19 13:50:48 +0530

    bo profit done

--- src/components/ProfitTable.jsx
+++ src/components/ProfitTable.jsx
@@ -18,48 +18,51 @@ import {
 
 const ProfitTable = () => {
   return (
-    <div className='flex items-center justify-center w-full ml-2 overflow-x-auto lg:w-full lg:ml-0 hide-scrollbar'>
-      <TableContainer component={Paper} sx={{bgcolor:"#DDDDDD", flexShrink:0}}>
-      <Table>
-        {/* Table Header */}
-        <TableHead>
-          <TableRow>
-            <TableCell>DATE</TableCell>
-            <TableCell>BROILER</TableCell>
-            <TableCell>LAYER</TableCell>
-            <TableCell>PARENTS</TableCell>
-            <TableCell>COUNTRY CHICKEN</TableCell>
-            <TableCell>TOTAL PROFIT (₹)</TableCell>
-          </TableRow>
-        </TableHead>
+    <div className='flex items-center justify-center w-full overflow-x-hidden mt-14 hide-scrollbar'>
+      <div className='w-48'></div>
+      <div className='flex items-center justify-center w-full lg:w-[85%] overflow-x-auto hide-scrollbar'>
+          <TableContainer component={Paper} sx={{bgcolor:"#DDDDDD", flexShrink:0, marginLeft:"1rem"}}>
+          <Table>
+            {/* Table Header */}
+            <TableHead>
+              <TableRow>
+                <TableCell>DATE</TableCell>
+                <TableCell>BROILER</TableCell>
+                <TableCell>LAYER</TableCell>
+                <TableCell>PARENTS</TableCell>
+                <TableCell>COUNTRY CHICKEN</TableCell>
+                <TableCell>TOTAL PROFIT (₹)</TableCell>
+              </TableRow>
+            </TableHead>
 
-        {/* Table Body */}
-        <TableBody>
-          {data.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell sx={{color:'#426CB4'}}>{row.date}</TableCell>
-              <TableCell>{row.broiler}</TableCell>
-              <TableCell>{row.layer}</TableCell>
-              <TableCell>{row.parents}</TableCell>
-              <TableCell>{row.chicken}</TableCell>
-              <TableCell>{row.profit}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+            {/* Table Body */}
+            <TableBody>
+              {data.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell sx={{color:'#426CB4'}}>{row.date}</TableCell>
+                  <TableCell>{row.broiler}</TableCell>
+                  <TableCell>{row.layer}</TableCell>
+                  <TableCell>{row.parents}</TableCell>
+                  <TableCell>{row.chicken}</TableCell>
+                  <TableCell>{row.profit}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
 
-        {/* Table Footer */}
-        <TableFooter>
-          <TableRow style={{ backgroundColor: '#1565c0', color: 'white' }}>
-            <TableCell style={{ color: 'white', fontWeight: 'bold' }}>TOTAL</TableCell>
-            <TableCell style={{ color: 'white' }}>{data[0].broiler}</TableCell>
-            <TableCell style={{ color: 'white' }}>{data[0].layer}</TableCell>
-            <TableCell style={{ color: 'white' }}>{data[0].parents}</TableCell>
-            <TableCell style={{ color: 'white' }}>{data[0].chicken}</TableCell>
-            <TableCell style={{ color: 'white' }}>{data[0].profit}</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+            {/* Table Footer */}
+            <TableFooter>
+              <TableRow style={{ backgroundColor: '#1565c0', color: 'white' }}>
+                <TableCell style={{ color: 'white', fontWeight: 'bold' }}>TOTAL</TableCell>
+                <TableCell style={{ color: 'white' }}>{data[0].broiler}</TableCell>
+                <TableCell style={{ color: 'white' }}>{data[0].layer}</TableCell>
+                <TableCell style={{ color: 'white' }}>{data[0].parents}</TableCell>
+                <TableCell style={{ color: 'white' }}>{data[0].chicken}</TableCell>
+                <TableCell style={{ color: 'white' }}>{data[0].profit}</TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   )
 }
