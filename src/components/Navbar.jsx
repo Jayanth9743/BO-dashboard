ce4c17c177f4f5214da591854f3d1fdb3faf56b7
Jayanth P <jayanthjayram6968@gmail.com>
2024-12-20 17:52:53 +0530

    table updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,50 @@
 import PropTypes from 'prop-types';
 import { BiMenuAltLeft } from "react-icons/bi";
 import { IoIosArrowBack } from "react-icons/io";
-import img from "../assets/porfile.jpeg"
+import img from "../assets/porfile.jpeg";
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
+import { useState } from 'react';
 
-const Navbar = ({toggleSidebar}) => {
+const Navbar = ({ toggleSidebar }) => {
+  const [dropDown, setDropDown] = useState(false);
+
+  const handleDropDown = () => {
+    setDropDown(!dropDown);
+  };
+
   return (
     <div className="fixed top-0 z-40 flex flex-col items-center justify-end w-full lg:flex-row ">
 
       <div className="flex justify-center text-lg lg:hidden font-semibold h-12 text-white items-center w-full bg-[#426CB4]">
-        <BiMenuAltLeft className="absolute z-40 text-3xl cursor-pointer left-1 top-2" onClick={toggleSidebar}/>
+        <BiMenuAltLeft className="absolute z-40 text-3xl cursor-pointer left-1 top-2" onClick={toggleSidebar} />
         <p>DashBoard</p>
       </div>
 
       <div className="flex items-center justify-between w-full h-12 bg-white lg:shadow-md">
         <div className="flex items-center justify-start w-full lg:w-[80%] gap-4 text-lg">
-          <IoIosArrowBack className="ml-2 text-xl lg:ml-56 lg:text-2xl "/>
+          <IoIosArrowBack className="ml-2 text-xl lg:ml-56 lg:text-2xl" />
           <p className="font-semibold lg:text-2xl">Profit</p>
         </div>
-        <div className=" hidden lg:flex justify-center items-center w-[30%] gap-2">
+        <div className="hidden lg:flex justify-end mr-6 items-center w-[30%] gap-2">
           <img src={img} alt="" className="object-contain w-8 h-8 rounded-full" />
-          <p className="mr-2 font-semibold">Manjunath Naik</p>
+          <div onClick={handleDropDown} className='relative flex items-center justify-center gap-1 cursor-pointer'>
+            <p className="mr-2 font-semibold">Manjunath Naik</p>
+            {dropDown ? <MdOutlineKeyboardArrowUp className='text-xl' /> : <MdOutlineKeyboardArrowDown className='text-xl' />}
+            {dropDown && (
+              <div className='absolute w-[8rem] mt-2 bg-white border border-gray-200 rounded-md shadow-lg top-full'>
+                <p className='p-2 cursor-pointer hover:bg-gray-100'>Profile Settings</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
     </div>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
   toggleSidebar: PropTypes.func.isRequired,
 };
 
-export default Navbar
+export default Navbar;
--- src/components/boPriceComponents/YearSelector.jsx
+++ src/components/boPriceComponents/YearSelector.jsx
@@ -1,37 +1,101 @@
+import { useState } from 'react';
 import data from "../../resources/DropDownData.json";
-//the data will change when the api is given
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { MdOutlineKeyboardArrowUp } from "react-icons/md";
+
+
 const YearSelector = () => {
-    console.log(data)
+  const [isYearDropdownOpen, setIsYearDropdownOpen] = useState(false);
+  const [isMonthDropdownOpen, setIsMonthDropdownOpen] = useState(false);
+  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedMonth, setSelectedMonth] = useState("");
+
+  const toggleYearDropdown = () => {
+    setIsYearDropdownOpen(!isYearDropdownOpen);
+  };
+
+  const toggleMonthDropdown = () => {
+    setIsMonthDropdownOpen(!isMonthDropdownOpen);
+  };
+
+  const handleYearSelect = (year) => {
+    setSelectedYear(year);
+    setIsYearDropdownOpen(false);
+  };
+
+  const handleMonthSelect = (month) => {
+    setSelectedMonth(month);
+    setIsMonthDropdownOpen(false);
+  };
+
   return (
     <div className="flex items-center justify-center w-full mt-8">
-
-        <div className='hidden w-48 lg:block'></div>
-
-        <div className="flex flex-col items-center justify-center w-full lg:w-[85%]">
-            <div className="flex flex-col items-center justify-center w-full gap-6 lg:gap-12 lg:flex-row">
-                <div className="flex flex-col items-start justify-center w-[85%] lg:w-[20rem] gap-2">
-                    <label htmlFor="drop-years" className="font-semibold">Choose Year</label>
-                    <select name="drop-years" id="drop-years" className="w-full cursor-pointer p-2 lg:p-1 bg-white border border-[#D5D7DA] rounded-md outline-none">
-                        <option value="" disabled selected>year</option>
-                        {data.year.map((year, index) => (
-                            <option key={index} value={year}>{year}</option>
-                        ))}
-                    </select>
+      <div className="hidden w-48 lg:block"></div>
+      <div className="flex flex-col items-center justify-center w-full lg:w-[85%]">
+        <div className="flex flex-col items-center justify-center w-full gap-6 lg:gap-12 lg:flex-row">
+          <div className="flex flex-col items-start justify-center w-[85%] lg:w-[20rem] gap-2">
+            <label htmlFor="drop-years" className="font-semibold">
+              Choose Year
+            </label>
+            <div className="relative w-full">
+              <div
+                className="w-full p-2 cursor-pointer lg:p-1 bg-white border border-[#D5D7DA] rounded-md"
+                onClick={toggleYearDropdown}
+              >
+                {selectedYear || "Year"}
+                <span className="absolute text-[#717680] text-2xl -translate-y-1/2 pointer-events-none right-2 top-1/2">
+                    {isYearDropdownOpen ? <MdOutlineKeyboardArrowUp /> : <MdOutlineKeyboardArrowDown /> }
+                </span>
+              </div>
+              {isYearDropdownOpen && (
+                <div className="absolute z-10 w-full mt-2 bg-white rounded-md shadow-lg" style={{ marginTop: '0.5rem' }}>
+                  {data.year.map((year, index) => (
+                    <div
+                      key={index}
+                      className="p-2 cursor-pointer hover:bg-[#426CB4] hover:text-white"
+                      onClick={() => handleYearSelect(year)}
+                    >
+                      {year}
+                    </div>
+                  ))}
                 </div>
-                <div className="flex flex-col items-start justify-center w-[85%] lg:w-[20rem] gap-2">
-                    <label htmlFor="drop-months" className="font-semibold" >Choose Month</label>
-                    <select name="drop-months" id="drop-months" className="w-full p-2 cursor-pointer lg:p-1 bg-white border border-[#D5D7DA] rounded-md outline-none">
-                        <option value="" disabled selected>Month</option>
-                        {data.months.map((month, index) => (
-                            <option key={index} value={month}>{month}</option>
-                        ))}
-                    </select>
-                </div>
+              )}
             </div>
-        </div>
-      
-    </div>
-  )
-}
+          </div>
 
-export default YearSelector
+          <div className="flex flex-col items-start justify-center w-[85%] lg:w-[20rem] gap-2">
+            <label htmlFor="drop-months" className="font-semibold">
+              Choose Month
+            </label>
+            <div className="relative w-full">
+              <div
+                className="w-full p-2 cursor-pointer lg:p-1 bg-white border border-[#D5D7DA] rounded-md outline-none"
+                onClick={toggleMonthDropdown}
+              >
+                {selectedMonth || "Month"}
+                <span className="absolute text-[#717680] text-2xl -translate-y-1/2 pointer-events-none right-2 top-1/2">
+                    {isMonthDropdownOpen ? <MdOutlineKeyboardArrowUp /> : <MdOutlineKeyboardArrowDown /> }
+                </span>
+              </div>
+              {isMonthDropdownOpen && (
+                <div className="absolute z-10 w-full mt-2 bg-white rounded-md shadow-lg" style={{ marginTop: '0.5rem' }}>
+                  {data.months.map((month, index) => (
+                    <div
+                      key={index}
+                      className="p-2 cursor-pointer hover:bg-[#426CB4] hover:text-white"
+                      onClick={() => handleMonthSelect(month)}
+                    >
+                      {month}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default YearSelector;
